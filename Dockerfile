69a418a6d5fa2087855d4568a83ab6f5ab4d7d2a
reba84 <rebecca.ruppel@gmail.com>
2019-05-21 16:38:17 -0500

    removed ubuntu image and changes for python workshop

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,21 @@
-FROM ubuntu:16.04
+FROM continuumio/miniconda3:4.5.11
 
-ENV HOME=/root
+ENV HOME=/app
 WORKDIR $HOME
 
-RUN apt-get update && apt-get install -y \
-    curl \
-    build-essential \
+RUN apt-get update && apt-get install -y --no-install-recommends \
     git
+# build-essential
 
-# install miniconda
-ENV PATH=$HOME/miniconda3/bin:$PATH
-RUN curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > /root/Miniconda3-latest-Linux-x86_64.sh && \
-    chmod u+x /root/Miniconda3-latest-Linux-x86_64.sh && \
-    /root/Miniconda3-latest-Linux-x86_64.sh -b && \
-    rm /root/Miniconda3-latest-Linux-x86_64.sh && \
-    conda update -n base conda
-
-# install packages listed in environment.yml including cpu-optimized tensorflow
 COPY environment.yml $HOME/
-RUN conda env create /root/environment.yml
-ENV PATH=$HOME/miniconda3/envs/workshop/bin:$PATH
+RUN conda update -n base conda && \
+    conda env create -f $HOME/environment.yml
 
+RUN echo "source activate workshop" > ~/.bashrc
+ENV PATH /opt/conda/envs/workshop/bin:$PATH
+
+
+#ENV PATH=$HOME/miniconda3/envs/workshop/bin:$PATH
 
 COPY . $HOME/
 
--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from flask import Flask, render_template
 import random
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 app = Flask(__name__)
 
-# list of cat images
-# images = []
+
 digits = load_digits()
 x_train, x_test, y_train, y_test = train_test_split(digits.data, digits.target, test_size=0.25, random_state=0)
-loaded_model = pickle.load(open("model/logreg_model.pkl", 'rb'))
+# loaded_model = pickle.load(open("model/logreg_model.pkl", 'rb'))
+image = x_test[200]
+image_res = np.array(image, dtype='float')
+pixels = image_res.reshape((8, 8))
+
+plt.imshow(pixels, cmap='gray')
+plt.axis('off')
+plt.savefig('static/number.png', bbox_inches='tight')
 
 @app.route('/')
 def index():
-    prediction = loaded_model.predict(x_test[100].reshape(1,-1))
-    static_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Friendly_pumpkin.jpg/128px-Friendly_pumpkin.jpg"
-    # static_image = random.choice(image)
-    return render_template('index.html', static_image=static_image, pred = prediction)
+    # prediction = loaded_model.predict(image.reshape(1,-1))
+    #, pred = prediction
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
